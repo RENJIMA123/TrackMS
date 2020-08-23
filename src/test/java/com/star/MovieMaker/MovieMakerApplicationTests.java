575721877aa99661baf26ff65d8d5b8d58bebdbb
unknown <you@example.com>
2020-08-23 13:37:31 +0530

    first commit

---
+++ src/test/java/com/star/MovieMaker/MovieMakerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.star.MovieMaker;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
